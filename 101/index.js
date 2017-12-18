9f7f84befcc4beaf4d086f3c52e01b90851ac4ba
Randy Champagne <champagne.randy@gmail.com>
2017-12-17 21:46:53 -0500

    now listening for disconnect event from socket io

--- 101/index.js
+++ 101/index.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+/* config Express routes */
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -10,10 +13,23 @@ app.get('/hello', function(req, res){
 	res.send('<h1>Hello world</h1>');
 });
 
+
+/* config Scoket.io event listeners */
+
 io.on('connection', function(socket){
-	console.log('a user connected');
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
+
+/* listen to specific ip */
 
 http.listen(3000, '10.0.0.91', function(){
 	console.log('listening on *:3000');
 });
+
+
+
+
