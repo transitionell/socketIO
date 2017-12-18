7b8db4564c368428a4aa35fab424ff08d034d644
Randy Champagne <champagne.randy@gmail.com>
2017-12-17 21:35:52 -0500

    successfully accessed app from other device

--- 101/index.js
+++ 101/index.js
@@ -1,10 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	//res.sendFile(__dirname + '/index.html');
+	res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+http.listen(3000, '10.0.0.91', function(){
 	console.log('listening on *:3000');
 });
